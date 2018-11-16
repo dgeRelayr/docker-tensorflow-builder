1471f6733ed7ffeedbc3f9797b1adf869afdf5f2
Dominik Geissler <dominik.geissler@relayr.io>
2018-11-16 10:52:50 +0100

    updates

--- tensorflow/ubuntu-16.04/build.sh
+++ tensorflow/ubuntu-16.04/build.sh
@@ -3,9 +3,6 @@ set -e
 
 export PATH="/conda/bin:/usr/bin:$PATH"
 
-if [ "$USE_GPU" -eq "1" ]; then
-	bash setup_cuda.sh
-fi
 
 gcc --version
 
@@ -13,7 +10,7 @@ gcc --version
 conda create --yes -n tensorflow python==$PYTHON_VERSION
 source activate tensorflow
 conda install --yes numpy wheel bazel
-conda install -c conda-forge keras-applications 
+conda install -c conda-forge keras-applications
 
 # Compile TensorFlow
 
@@ -36,11 +33,11 @@ export PYTHON_ARG=${TF_ROOT}/lib
 
 # Compilation parameters
 export TF_NEED_CUDA=0
-export TF_NEED_GCP=1
+export TF_NEED_GCP=0
 export TF_CUDA_COMPUTE_CAPABILITIES=5.2,3.5
-export TF_NEED_HDFS=1
+export TF_NEED_HDFS=0
 export TF_NEED_OPENCL=0
-export TF_NEED_JEMALLOC=1  # Need to be disabled on CentOS 6.6
+export TF_NEED_JEMALLOC=0  # Need to be disabled on CentOS 6.6
 export TF_ENABLE_XLA=0
 export TF_NEED_VERBS=0
 export TF_CUDA_CLANG=0
@@ -48,8 +45,8 @@ export TF_DOWNLOAD_CLANG=0
 export TF_NEED_MKL=0
 export TF_DOWNLOAD_MKL=0
 export TF_NEED_MPI=0
-export TF_NEED_S3=1
-export TF_NEED_KAFKA=1
+export TF_NEED_S3=0
+export TF_NEED_KAFKA=0
 export TF_NEED_GDR=0
 export TF_NEED_OPENCL_SYCL=0
 export TF_SET_ANDROID_WORKSPACE=0
@@ -59,38 +56,12 @@ export TF_NEED_AWS=0
 export GCC_HOST_COMPILER_PATH=$(which gcc)
 export CC_OPT_FLAGS="-march=native"
 
-if [ "$USE_GPU" -eq "1" ]; then
-	# Cuda parameters
-	export CUDA_TOOLKIT_PATH=/usr/local/cuda
-	export CUDNN_INSTALL_PATH=/usr/local/cuda
-	export TF_CUDA_VERSION="$CUDA_VERSION"
-	export TF_CUDNN_VERSION="$CUDNN_VERSION"
-	export TF_NEED_CUDA=1
-	export TF_NEED_TENSORRT=0
-	export TF_NCCL_VERSION=1.3
-
-	# Those two lines are important for the linking step.
-	export LD_LIBRARY_PATH="$CUDA_TOOLKIT_PATH/lib64:${LD_LIBRARY_PATH}"
-	ldconfig
-fi
-
 # Compilation
 ./configure
 
-if [ "$USE_GPU" -eq "1" ]; then
-
-	bazel build --config=opt \
-	    		--config=cuda \
-	    		--action_env="LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" \
-	    		//tensorflow/tools/pip_package:build_pip_package
-
-else
-
-	bazel build --config=opt \
-			    --action_env="LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" \
-			    //tensorflow/tools/pip_package:build_pip_package
-
-fi
+bazel build --config=opt \
+		    --action_env="LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" \
+		    //tensorflow/tools/pip_package:build_pip_package
 
 # Project name can only be set for TF > 1.8
 #PROJECT_NAME="tensorflow_gpu_cuda_${TF_CUDA_VERSION}_cudnn_${TF_CUDNN_VERSION}"
